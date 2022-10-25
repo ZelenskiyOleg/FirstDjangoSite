a98f0099a48ba6646553b037438cc01f6117e4a6
NTComp <zelenskiyoleg2000@gmail.com>
2022-10-25 16:52:46 +0300

    Add Text reverse

--- viceversa/views.py
+++ viceversa/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    reversed_text = reverse_string(user_text)
+    return render(request, 'reverse.html', {'usertext':user_text,
+                                            'reversed_text':reversed_text})
+
+def reverse_string(s):
+    return s[::-1]
